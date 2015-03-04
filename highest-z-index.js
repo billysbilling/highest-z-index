adecb374d7e4e4b0dabd6fde55eaf6053d674e77
Sebastian Seilund <sebastian@seilund.com>
2015-03-04 15:29:05 -0800

    Added setMin() to force z-indexes to be at minimum a certain value instead of starting at 1

--- highest-z-index.js
+++ highest-z-index.js
@@ -1,19 +1,28 @@
 var $ = require('jquery');
 
+var min = 1
+
 module.exports = function(els) {
     els = $(els || 'body > *');
 
+    var zIndex
     if (els.length === 0) {
-        return 1;
+        zIndex = 1
+    } else {
+        var zIndexes = $.map(els, function (el) {
+            var $el = $(el);
+            if ($el.css('position') !== 'static' && $el.is(':visible')) {
+                return Number($el.css('z-index')) || 1;
+            }
+            return 1;
+        });
+
+        zIndex = Math.max.apply(null, zIndexes);
     }
 
-    var zIndexes = $.map(els, function (el) {
-        var $el = $(el);
-        if ($el.css('position') !== 'static' && $el.is(':visible')) {
-            return Number($el.css('z-index')) || 1;
-        }
-        return 1;
-    });
-
-    return Math.max.apply(null, zIndexes);
+    return Math.max(zIndex, min)
 };
+
+module.exports.setMin = function(v) {
+    min = v
+}
